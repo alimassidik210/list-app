21b098fc66ba17ff3ee0f92fd7b1f33b2fbf9e64
alimassidik210 <alim.assidik210@gmail.com>
2024-07-10 13:15:19 +0700

    update the width size for mobile in the Form component

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ export default function App() {
   }
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col  min-h-screen">
       <Header
         titleFormChild={titleFormChild}
         onChangeTitle={handleChangeTitle}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,33 +23,33 @@ export default function Form({ onAddItem, titleFormChild }) {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex p-4  flex-col sm:flex-row  justify-center items-center gap-2 bg-green-400"
+      className="flex p-4 flex-col sm:flex-row  justify-center items-center gap-2 bg-green-400"
     >
-      <h3 className="text-2xl text-center">
+      <h3 className="text-xl text-center">
         Hari ini {titleFormChild} apa kita 🤔 ?
       </h3>
-      <div className="flex gap-2 justify-center">
-        <input
-          type="text"
-          className="p-2 text-center bg-gray-200 rounded-md w-36 text-xs"
-          placeholder="jumlah / jenis (optional)"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        />
-        <input
-          type="text"
-          className="p-2 text-center bg-gray-200 rounded-md"
-          placeholder="masukan nama list"
-          value={name}
-          min={2}
-          max={20}
-          required
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button className="bg-blue-500 text-xs w-12 uppercase p-2 rounded-md text-white hover:opacity-85 ">
-          Add
-        </button>
-      </div>
+      {/* <div className="flex gap-2 px-3 justify-center"> */}
+      <input
+        type="text"
+        className="p-2 text-center bg-gray-200 rounded-md w-full md:w-1/3 "
+        placeholder="jumlah / jenis (optional)"
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
+      />
+      <input
+        type="text"
+        className="p-2 text-center bg-gray-200 w-full md:w-1/3 rounded-md"
+        placeholder="masukan nama list"
+        value={name}
+        min={2}
+        max={20}
+        required
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button className="bg-blue-500 text-xs w-full md:w-auto uppercase p-2 rounded-md text-white hover:opacity-85 ">
+        Add
+      </button>
+      {/* </div> */}
     </form>
   );
 }
